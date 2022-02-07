bbcd3fe1800f2aede66afe56f54dd68ad6803019
jonySh2020 <80696490+jonySh2020@users.noreply.github.com>
2022-02-07 12:03:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './components/loginButton';
+import LogoutButton from './components/logoutButton';
+import Profile from './components/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function App() {
+  const { isLoading }=useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <>
+    <LoginButton />
+    <LogoutButton />
+    <Profile />
+    </>
+    
+  );
+}
+
+export default App;
